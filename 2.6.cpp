3ce874643a2ba1c570edbb537eeb6b8070b7036b
ngoc-dan <ngocdan191026@gmail.com>
2024-11-22 10:37:06 +0700

    Add files via upload

---
+++ 2.6.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int r=3;
+	const float pi=3.14;
+	float C,D;
+	C=2*pi*r;
+	D=pi*r*r;
+	printf("chu vi hinh tron la %2f\n",C);
+	printf("dien tich hinh ytron la %2f\n",D);
+	
+	return 0;
+}
